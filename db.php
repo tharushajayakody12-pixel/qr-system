6861787fc3167eb69fb85fadaaeaabd043e0e824
tharushajayakody12-pixel <tharushajayakody12@gmail.com>
2026-04-06 12:33:18 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,7 +1,12 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "qr_system");
+$host = getenv("MYSQLHOST");
+$user = getenv("MYSQLUSER");
+$pass = getenv("MYSQLPASSWORD");
+$db   = getenv("MYSQLDATABASE");
+
+$conn = new mysqli($host, $user, $pass, $db);
 
 if ($conn->connect_error) {
-    die("Connection failed");
+    die("Connection failed: " . $conn->connect_error);
 }
 ?>
